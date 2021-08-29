daa26b087801a7f23b4dca1dec1718108b629c81
Chawn Neal <chawnneal@gmail.com>
2021-08-29 17:35:50 -0500

    setup FactoryBot and member factory

---
+++ spec/factories/member.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+    factory :member do
+        sequence(:first_name) { |i| "first_name_#{i}"}
+        sequence(:last_name) { |i| "last_name_#{i}"}
+        sequence(:email) { |i| "email#{i}@gmail.com"}
+        password {"password"}
+        password_confirmation {"password"}
+    end
+end
